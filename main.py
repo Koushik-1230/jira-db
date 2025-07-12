b85c409b452084cccfa67decdccd365e01df09d9
Koushik-1230 <koushikrekulapally@gmail.com>
2025-07-12 16:36:51 +0100

    updating checks

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ import requests
 from modules import api_client, logger
 
 
-
-
 def main():
     log = logger.Logger()
     log.info("Starting Jira API client...")
+
 
 if __name__ == "__main__":
     main()
