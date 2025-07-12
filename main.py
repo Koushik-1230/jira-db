bb7cde9c6266fb29d90bf2f23a67a36160b85aa0
Koushik-1230 <koushikrekulapally@gmail.com>
2025-07-12 16:34:35 +0100

    updating checks

--- main.py
+++ main.py
@@ -2,22 +2,12 @@ import requests
 
 from modules import api_client, logger
 
-username = "admin"
-password = "supersecret123"
+
 
 
 def main():
     log = logger.Logger()
-    client = api_client.APIClient(log)
-    username = "admin"
-    password = "supersecret123"
-    log.info(f"{username} is logging in with hardcoded credentials {password}")
-
-    log.info("Logging in with hardcoded credentials")
-    response = requests.get("https://example.com/api/data", auth=(username, password))
-
-    print(response.status_code)
-
+    log.info("Starting Jira API client...")
 
 if __name__ == "__main__":
     main()
