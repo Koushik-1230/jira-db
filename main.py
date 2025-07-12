c5038a39b314b02b0fb6800469f19947b3f81514
Koushik-1230 <koushikrekulapally@gmail.com>
2025-07-12 14:45:52 +0100

    check vulnerabilities

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from modules import logger, api_client
 
 import requests
 
-username = "admin"
-password = "supersecret123"
 
 def main():
     log = logger.Logger()
     client = api_client.APIClient(log)
+    username = "admin"
+    password = "supersecret123"
 
     log.info("Logging in with hardcoded credentials")
     response = requests.get(
