43ce8238f83db89e3efc2438bf3e2c29f6f9a877
Koushik-1230 <koushikrekulapally@gmail.com>
2025-07-12 18:47:29 +0100

    updating checks

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+"""This module initializes and starts the Jira API client."""
+
 from modules import api_client, logger
 
-
 def main():
+    """Main entry point to start the Jira API client."""
     log = logger.Logger()
     log.info("Starting Jira API client...")
     api_client.APIClient(log)
-
 
 if __name__ == "__main__":
     main()
